9c42e47cd217dcf4b2bb007b7de99d4d430745df
Alexeuss82 <krukovalex@gmail.com>
2025-05-03 12:51:51 +0300

    test weight commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 57;
+        int weight = 66;
         double height = 1.66;
         double bodyMassIndex = service.calculate(weight,height);
         System.out.println("Ваш индекс массы тела = " + bodyMassIndex + " !");
